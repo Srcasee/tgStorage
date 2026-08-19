851e3619ea1f1a14599c1a35bf795915e0ec1ba0
青青子衿 <srcasese@gmail.com>
2026-08-20 00:26:52 +0800

    feat(v2): expose resource search API

--- app/v2/api/router.py
+++ app/v2/api/router.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from app.v2.api.download import router as download_router
+from app.v2.api.resources import router as resources_router
 
 router = APIRouter(prefix="/api/v2")
 router.include_router(download_router)
+router.include_router(resources_router, prefix="", tags=["resources"])
