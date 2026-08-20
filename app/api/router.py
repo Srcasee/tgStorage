09e3438eb9fd89a8e026b7b66e4d757291ff9128
青青子衿 <srcasese@gmail.com>
2026-08-20 19:14:13 +0800

    feat: register admin api router

--- app/api/router.py
+++ app/api/router.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
+from app.api.admin import router as admin_router
 from app.api.download import router as download_router
 from app.api.resources import router as resources_router
 
 router = APIRouter(prefix="/api/v2")
 router.include_router(resources_router)
 router.include_router(download_router)
+router.include_router(admin_router)
