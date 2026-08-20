196868e00f71d05087fa630187b132a4e73b688d
青青子衿 <srcasese@gmail.com>
2026-08-21 00:16:55 +0800

    fix(login): support custom telegram account names

--- login-account.sh
+++ login-account.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eu
 
-ACCOUNT_NAME="${1:?用法: ./login-account.sh <session_name>}"
-PHONE="${2:?用法: ./login-account.sh <session_name> <phone>}"
+ACCOUNT_NAME="${1:?用法: ./login-account.sh <account_name> <phone>}"
+PHONE="${2:?用法: ./login-account.sh <account_name> <phone>}"
 
 SESSION_DIR="${TG_SESSION_DIR:-./data/accounts}"
 SESSION_PATH="${SESSION_DIR}/${ACCOUNT_NAME}"
@@ -17,4 +17,4 @@ docker compose run --rm \
   -e TG_PHONE="${PHONE}" \
   -e TG_SESSION="${SESSION_PATH}" \
   telegram-drive \
-  python3 /app/telegram/login.py
+  python3 -m app.telegram.login
