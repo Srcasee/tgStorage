60b0ae6dfe0b59acba4e1ca4346a2705578e46ec
青青子衿 <srcasese@gmail.com>
2026-08-20 17:59:52 +0800

    test: align classifier expectations with video category rules

--- tests/test_classifier.py
+++ tests/test_classifier.py
@@ -11,6 +11,6 @@ def test_explicit_anime_keyword_classifies_as_anime():
     assert classifier.classify("One.Piece.anime.1080p.mkv", "video", ["1080p"]) == "动漫"
 
 
-def test_resolution_is_only_a_tag_when_no_category_rule_matches():
+def test_resolution_is_only_a_tag_when_no_explicit_category_matches():
     classifier = ResourceClassifier(rules=[])
-    assert classifier.classify("movie.1080p.mkv", "video", ["1080p"]) is None
+    assert classifier.classify("movie.1080p.mkv", "unknown", ["1080p"]) is None
